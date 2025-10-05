f8be760e101166cd5ff4e921bd0d5c8d93fa9c80
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2025-10-05 14:23:13 -0400

    feat: create and configure main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+        try {
+            let reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+
+            console.log('Service worker registered successfully!', reg);
+        } catch (err) {
+            console.log('Service worker registration failed.', err);
+        }
+    });
+}
