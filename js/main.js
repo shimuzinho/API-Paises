5623a117e883f6e5f183064b3a84e3a44d3cd6ab
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2025-10-07 16:19:11 -0400

    feat(countries): add search and display all countries with styling

--- js/main.js
+++ js/main.js
@@ -10,10 +10,42 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-const createPais = (pais) => {
+const searchPais = async () => {
+    const input = document.querySelector('#search').value;
+    const pais = await fetch(`https://restcountries.com/v3.1/name/${input}`)
+        .then(res => res.json())
+        .then(res => res[0])
+        .catch(err => {
+            console.log(err);
+            return err;
+        });
+    if (!pais) {
+        alert('País Inválido.');
+        return;
+    }
+    const saida = document.querySelector('#containerPaises');
+    saida.innerHTML = createPais(pais, 'containerPaisUnico');
+}
+
+const showAllPaises = async () => {
+    const saida = document.querySelector('#containerPaises');
+    const paises = await fetch('https://restcountries.com/v3.1/region/america')
+        .then(res => res.json())
+        .catch(err => {
+            console.log(err);
+            return err;
+        });
+    saida.innerHTML = paises.map(createPais).join('');
+}
+
+const createPais = (pais, classe = '') => {
     return `
-    <div>
-        <img src="${pais.flags.png}" alt="Bandeira do Brasil" />
+    <div class="containerPais ${classe}">
+        <img class="imagemPais" src="${pais.flags.png}" alt="Bandeira do ${pais.name.common}" />
+        <p class="nomePais">País: ${pais.name.common}</p>
+        <p class="capitalPais">Capital: ${pais.capital[0]}</p>
     </div>
     `;
 }
+
+showAllPaises();
